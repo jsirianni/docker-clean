b3cd4d48d99499b8c5bbec1b465dde95d261942a
jsirianni <joe.sirianni@bluemedora.com>
2022-01-14 10:19:24 -0500

    rename script. kill and delete all running containers

--- clean-docker.sh
+++ clean-docker.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+kill_all() {
+    docker ps -q | xargs -I{} docker rm -f {}
+}
+
 prune_system() {
     docker system prune --force --all --volumes
     echo "cleaned containers, volumes, networks, and images."
